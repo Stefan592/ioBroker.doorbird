c09da21eb17e1de0e00ca4f20e79d811eed9068f
Stefan Preuß <stefan@preuss-automation.de>
2026-03-19 14:00:48 +0100

    adapt to my needs

--- main.js
+++ main.js
@@ -45,7 +45,11 @@ class Doorbird extends utils.Adapter {
 
         this.wizard = false;
         this.wizardTimeout = null;
+
+        this.config.adapterAddress = '172.29.178.42'
     }
+
+
 
     /**
      * Is called when databases are connected and adapter received configuration.
@@ -110,9 +114,9 @@ class Doorbird extends utils.Adapter {
                     }
                 });
 
-                this.server.listen(this.config.adapterport || 8081, this.config.adapterAddress, () => {
+                this.server.listen(this.config.adapterport || 8081, () => {
                     this.log.debug(
-                        `Server started on port ${this.config.adapterport || 8100} with IP ${this.config.adapterAddress}`,
+                        `Server started on port ${this.config.adapterport || 8100} and listen on all interfaces.`,
                     );
                 });
             } catch (e) {
